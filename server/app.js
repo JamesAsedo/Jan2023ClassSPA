ee32b612d694811f347e2bf5ef17f4991bedd58f
James Asedo <james.asedo@gmail.com>
2023-03-07 13:08:43 -0800

    Class SPA updates

--- server/app.js
+++ server/app.js
@@ -1,10 +1,22 @@
 // 'Import' the Express module instead of http
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const pizzas = require("./routers/pizzas");
 
 dotenv.config();
 
 const PORT = process.env.PORT || 4040; // we use || to provide a default value
+// Initialize the Express application
+mongoose.connect(process.env.MONGODB);
+const db = mongoose.connection;
+
+db.on("error", console.error.bind(console, "Connection Error:"));
+db.once(
+  "open",
+  console.log.bind(console, "Successfully opened connection to Mongo!")
+);
+
 // Initialize the Express application
 const app = express();
 
@@ -18,9 +30,9 @@ app.use(logging);
 
 // Handle the request with HTTP GET method from http://localhost:4040/status
 app.get("/status", (request, response) => {
-   // Create the headers for response by default 200
-   // Create the response body
-   // End and return the response
+  // Create the headers for response by default 200
+  // Create the response body
+  // End and return the response
   response.status(418).json({ message: "Service healthy" });
 });
 
@@ -44,10 +56,12 @@ app.post("/add", (request, response) => {
   const num1 = request.body.numberOne;
   const num2 = request.body.numberTwo;
   const responseBody = {
-     sum: num1 + num2
+    sum: num1 + num2
   };
   response.json(responseBody);
 });
+
+app.use("/pizzas", pizzas);
 
 // Tell the Express app to start listening
 // Let the humans know I am running and listening on 4040
